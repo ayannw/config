1d0ae064b999d69a27fba216c1fa0094e63da8f6
ayannw <ayanispro420@gmail.com>
2021-12-31 09:13:41 +0600

    upload .config

---
+++ .config/fish/functions/fish_greeting.fish
@@ -0,0 +1,7 @@
+function fish_greeting
+  clear
+  cfetch
+  echo
+  echo (set_color 515152)(uname -a)(set_color normal)
+end
+
---
+++ .config/fish/functions/lx.fish
@@ -0,0 +1,4 @@
+function lx
+  exa --tree --icons --long --no-user --no-permissions --header --tree -F $argv
+end
+
